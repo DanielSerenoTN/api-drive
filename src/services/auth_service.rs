3984e5771bbe9f15a623f7b5964a00ccbe9cd577
DanielSerenoTN <daniel.marin@tendril.us>
2024-10-22 14:39:42 -0600

    feat: auth service  with dependency injection for token handling

---
+++ src/services/auth_service.rs
@@ -0,0 +1,59 @@
+use actix_web::{web,HttpResponse, Responder};
+use serde::Deserialize;
+use crate::auth::auth::{build_auth_url, TokenResponse};
+use crate::config::Config;
+use std::future::Future;
+use std::error::Error;
+use std::pin::Pin;
+
+#[derive(Deserialize)]
+pub struct AuthCallbackQuery {
+    pub code: String,
+}
+
+pub trait AuthService {
+    fn get_access_token<'a>(
+        &'a self,
+        code: &'a str,
+        config: &'a Config
+    ) -> Pin<Box<dyn Future<Output = Result<TokenResponse, Box<dyn Error>>> + 'a>>;  // Utiliza Pin<Box<...>>
+}
+
+pub struct AuthTokenService;
+
+impl AuthService for AuthTokenService {
+    fn get_access_token<'a>(
+        &'a self,
+        code: &'a str,
+        config: &'a Config
+    ) -> Pin<Box<dyn Future<Output = Result<TokenResponse, Box<dyn Error>>> + 'a>> {
+        Box::pin(crate::auth::auth::get_access_token(code, config))
+    }
+}
+
+pub async fn auth_callback<T: AuthService>(
+    query: web::Query<AuthCallbackQuery>,
+    config: web::Data<Config>,
+    token_service: web::Data<T>,          
+) -> impl Responder {
+    match token_service.get_access_token(&query.code, &config).await {
+        Ok(token_response) => {
+            HttpResponse::Ok().body(format!(
+                "Access Token: {}\nExpires in: {}",
+                token_response.access_token,
+                token_response.expires_in
+            ))
+        }
+        Err(err) => {
+            HttpResponse::InternalServerError().body(format!("Error obtaining token: {:?}", err))
+        }
+    }
+}
+
+pub async fn get_auth_url(config: web::Data<Config>) -> String {
+    build_auth_url(config)
+}
+
+
+
+
