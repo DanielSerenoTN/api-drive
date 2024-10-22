37a7794c29466b5bed08664fa1e8f4e240bdcba7
DanielSerenoTN <daniel.marin@tendril.us>
2024-10-22 15:01:25 -0600

    feat: add unit tests for auth service with mocked token exchange

---
+++ tests/mocks/auth_service_mock.rs
@@ -0,0 +1,37 @@
+use api_drive::config::Config;
+use api_drive::auth::auth::TokenResponse;
+use api_drive::services::auth_service::AuthService;
+use std::future::Future;
+use std::pin::Pin;
+use std::error::Error;
+
+pub struct MockAuthService;
+
+impl AuthService for MockAuthService {
+    fn get_access_token<'a>(
+        &'a self,
+        _code: &'a str,
+        _config: &'a Config
+    ) -> Pin<Box<dyn Future<Output = Result<TokenResponse, Box<dyn Error>>> + 'a>> {
+        Box::pin(async {
+            Ok(TokenResponse {
+                access_token: "mock_access_token".to_string(),
+                expires_in: 3600,
+            })
+        })
+    }
+}
+
+pub struct MockAuthServiceError;
+
+impl AuthService for MockAuthServiceError {
+    fn get_access_token<'a>(
+        &'a self,
+        _code: &'a str,
+        _config: &'a Config
+    ) -> Pin<Box<dyn Future<Output = Result<TokenResponse, Box<dyn Error>>> + 'a>> {
+        Box::pin(async {
+            Err(Box::from("Mock error exchanging code for token"))
+        })
+    }
+}
