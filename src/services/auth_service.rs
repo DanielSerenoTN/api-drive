68bc5d901ce16224cb0c429dfdc7842fa7f7d068
DanielSerenoTN <daniel.marin@tendril.us>
2024-10-22 20:45:42 -0600

    refactor: move auth logic to handlers for better separation of concerns

--- src/services/auth_service.rs
+++ src/services/auth_service.rs
@@ -1,6 +1,5 @@
-use actix_web::{web,HttpResponse, Responder};
 use serde::Deserialize;
-use crate::api::auth::{build_auth_url, get_access_token, TokenResponse};
+use crate::api::auth::{get_access_token, TokenResponse};
 use crate::config::Config;
 use std::future::Future;
 use std::error::Error;
@@ -31,28 +30,9 @@ impl AuthService for AuthTokenService {
     }
 }
 
-pub async fn auth_callback<T: AuthService>(
-    query: web::Query<AuthCallbackQuery>,
-    config: web::Data<Config>,
-    token_service: web::Data<T>,          
-) -> impl Responder {
-    match token_service.get_access_token(&query.code, &config).await {
-        Ok(token_response) => {
-            HttpResponse::Ok().body(format!(
-                "Access Token: {}\nExpires in: {}",
-                token_response.access_token,
-                token_response.expires_in
-            ))
-        }
-        Err(err) => {
-            HttpResponse::InternalServerError().body(format!("Error obtaining token: {:?}", err))
-        }
-    }
-}
 
-pub async fn get_auth_url(config: web::Data<Config>) -> String {
-    build_auth_url(config)
-}
+
+
 
 
 
