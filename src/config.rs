f29ad6d1b3b9c9528c2d934924be12a29c153a49
Daniel  Sereno <119629517+DanielSerenoTN@users.noreply.github.com>
2024-10-24 09:00:08 -0600

    simplify .env by hardcoding default URLs in config

--- src/config.rs
+++ src/config.rs
@@ -5,13 +5,12 @@ pub struct Config {
     pub client_id: String,
     pub client_secret: String,
     pub redirect_uri: String,
-    pub auth_uri: String,
-    pub token_uri: String,
     pub scope: String,
     pub serv_addrs: String,
     pub drive_api_base_url: String,
     pub drive_upload_url: String,
-    pub token_validation_url: String,
+    pub auth_uri: &'static str,
+    pub token_uri: &'static str,
 }
 
 impl Config {
@@ -21,24 +20,24 @@ impl Config {
         let client_id = env::var("CLIENT_ID").expect("Client ID is missing.");
         let client_secret = env::var("CLIENT_SECRET").expect("Client secret is missing.");
         let redirect_uri = env::var("REDIRECT_URI").expect("Redirect URL is missing.");
-        let auth_uri = env::var("AUTH_URI").expect("Auth URL is missing.");
-        let token_uri = env::var("TOKEN_URI").expect("Token URL is missing.");
-        let scope = env::var("SCOPE").expect("Scope is missing.");
+        let scope = env::var("SCOPE").unwrap_or_else(|_| "https://www.googleapis.com/auth/drive".to_string());
         let serv_addrs = env::var("SERV_ADDRS").expect("Server address is missing.");
-        let drive_api_base_url = env::var("GOOGLE_DRIVE_API_BASE_URL").expect("Google drive api URL is missing.");
-        let drive_upload_url = env::var("GOOGLE_DRIVE_UPLOAD_URL").expect("Google drive upload URL is missing.");
-        let token_validation_url = env::var("TOKEN_VALIDATION_URL").expect("Token URL validation is missing.");
+        let drive_api_base_url = env::var("GOOGLE_DRIVE_API_BASE_URL").unwrap_or_else(|_| "https://www.googleapis.com/drive/v3/files".to_string());
+        let drive_upload_url = env::var("GOOGLE_DRIVE_UPLOAD_URL").unwrap_or_else(|_| "https://www.googleapis.com/upload/drive/v3/files".to_string());
+     
+        let auth_uri = "https://accounts.google.com/o/oauth2/auth";
+        let token_uri = "https://oauth2.googleapis.com/token";
+
         Config {
             client_id,
             client_secret,
             redirect_uri,
-            auth_uri,
-            token_uri,
             scope,
             serv_addrs,
             drive_api_base_url,
             drive_upload_url,
-            token_validation_url
+            auth_uri,
+            token_uri,
         }
     }
 }
