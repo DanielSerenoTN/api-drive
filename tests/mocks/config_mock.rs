99d97197e1a3170a8e900a604a2596fc24d8a30c
DanielSerenoTN <daniel.marin@tendril.us>
2024-10-24 11:55:00 -0600

    fix mock config

--- tests/mocks/config_mock.rs
+++ tests/mocks/config_mock.rs
@@ -5,12 +5,11 @@ pub fn mock_config() -> Config {
         client_id: "test_client_id".to_string(),
         client_secret: "test_secret".to_string(),
         redirect_uri: "http://localhost:8080/callback".to_string(),
-        auth_uri: "https://accounts.google.com/o/oauth2/auth".to_string(),
-        token_uri: "https://oauth2.googleapis.com/token".to_string(),
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
         scope: "https://www.googleapis.com/auth/drive".to_string(),
         serv_addrs: "127.0.0.1:8080".to_string(),
         drive_api_base_url: "https://www.googleapis.com/drive/v3/files".to_string(),
         drive_upload_url: "https://www.googleapis.com/upload/drive/v3/files".to_string(),
-        token_validation_url: "https://www.googleapis.com/oauth2/v3".to_string(),
     }
 }
